9260068dd79d1c3fc9be909cb9748d5af5579249
Kajen01 <nkajenthiran22@gmail.com>
2024-10-26 09:41:12 +0530

    Signup, Update .js

---
+++ script.js
@@ -0,0 +1,84 @@
+// Header section
+const navigators = document.getElementsByClassName('navigators')[0]
+
+function navigatorFun() {
+    // Hide the navigators
+    if (navigators.style.display === 'block') {
+        navigators.style.display = 'none';
+    }
+    // Show the navigators
+    else {
+        navigators.style.display = 'block';
+    }
+}
+// Reload the page
+function refreshOnResize() {
+    window.location.reload();
+}
+window.addEventListener('resize', refreshOnResize);
+
+
+// User section
+const blurbackground = document.getElementsByClassName('blurbackground')[0]
+const signin = document.getElementsByClassName('signin')[0]
+const signup = document.getElementsByClassName('signup')[0]
+const forget = document.getElementsByClassName('forget')[0]
+const sendmessage = document.getElementsByClassName('sendmessage')[0]
+
+function signinFun() {
+    if (blurbackground.style.display === 'block') {
+        doneFun()
+    }
+    else {
+        blurbackground.style.display = 'block'
+        signin.style.display = 'block'
+        signup.style.display = 'none'
+        forget.style.display = 'none'
+        sendmessage.style.display = 'none'
+    }
+}
+
+function signupFun() {
+    blurbackground.style.display = 'block'
+    signin.style.display = 'none'
+    signup.style.display = 'block'
+    forget.style.display = 'none'
+    sendmessage.style.display = 'none'
+}
+
+function submitFun() {
+    blurbackground.style.display = 'none'
+    signin.style.display = 'none'
+    signup.style.display = 'none'
+    forget.style.display = 'none'
+    sendmessage.style.display = 'none'
+}
+
+function forgetFun() {
+    blurbackground.style.display = 'block'
+    signin.style.display = 'none'
+    signup.style.display = 'none'
+    forget.style.display = 'block'
+    sendmessage.style.display = 'none'
+}
+
+function sendFun() {
+    blurbackground.style.display = 'block'
+    signin.style.display = 'none'
+    signup.style.display = 'none'
+    forget.style.display = 'none'
+    sendmessage.style.display = 'block'
+}
+
+function doneFun() {
+    blurbackground.style.display = 'none'
+    signin.style.display = 'none'
+    signup.style.display = 'none'
+    forget.style.display = 'none'
+    sendmessage.style.display = 'none'
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+}
+
